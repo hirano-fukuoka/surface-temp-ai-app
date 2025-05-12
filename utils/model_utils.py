6567e7459870a1dd5290cb6c2a157ced96137e4b
hirano-fukuoka <58233549+hirano-fukuoka@users.noreply.github.com>
2025-05-12 18:10:39 +0900

    Update model_utils.py

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -1,5 +1,14 @@
-import os
 import torch
+from datetime import datetime
+import os
+
+def save_model_by_date(model, dir="model/"):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    date_str = datetime.now().strftime("%Y%m%d")
+    path = os.path.join(dir, f"lstm_{date_str}.pt")
+    torch.save(model.state_dict(), path)
+    return path
 
 def load_latest_model(model_class, model_dir="model/"):
     model_files = sorted(
@@ -7,7 +16,7 @@ def load_latest_model(model_class, model_dir="model/"):
         reverse=True
     )
     if not model_files:
-        raise FileNotFoundError("モデルが見つかりません。再学習してください。")
+        raise FileNotFoundError("モデルが見つかりません")
     latest_path = os.path.join(model_dir, model_files[0])
     model = model_class()
     model.load_state_dict(torch.load(latest_path, map_location="cpu"))
